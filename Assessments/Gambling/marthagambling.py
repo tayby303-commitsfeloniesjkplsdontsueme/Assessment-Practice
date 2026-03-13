b63e62949cf0435d0e46390d7f7ec2d9042f8af1
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-03-13 10:15:01 -0400

    commit

--- Assessments/Gambling/marthagambling.py
+++ Assessments/Gambling/marthagambling.py
@@ -10,24 +10,25 @@ def gambling(quarters,lastpaidt1,lastpaidt2,lastpaidt3):
     s1-=lastpaidt1
     s2-=lastpaidt2
     s3-=lastpaidt3
-    while quarters>=0:
-        s1-=1
-        quarters-=1
-        timeplayed+=1
-        if s1==0:
-            quarters+=30
-            s1+=35
-        s2-=1
-        quarters-=1
-        timeplayed+=1
-        if s2==0:
-            quarters+=60
-            s2+=100
-        s3-=1
-        quarters-=1
-        timeplayed+=1
-        if s3==0:
-            quarters+=9
-            s3+=10
-    print(f"Martha played {timeplayed} times before going broke.")
+    while quarters>0:
+        if quarters == 0:
+            s1-=1
+            quarters-=1
+            timeplayed+=1
+            if s1==0:
+                quarters+=30
+                s1+=35
+            s2-=1
+            quarters-=1
+            timeplayed+=1
+            if s2==0:
+                quarters+=60
+                s2+=100
+            s3-=1
+            quarters-=1
+            timeplayed+=1
+            if s3==0:
+                quarters+=9
+                s3+=10
+    print(f"Martha played {timeplayed} times before going broke.")  
 gambling(quarters,lastpaidt1,lastpaidt2,lastpaidt3)
